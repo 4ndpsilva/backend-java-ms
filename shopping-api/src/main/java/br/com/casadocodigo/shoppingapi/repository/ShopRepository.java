e78dac321871c8303c640e3822a9f9a1e226edf6
André Pereira da Silva <4ndpsilva@gmail.com>
2023-02-24 05:20:53 -0300

    Adição de métodos de pesquisa no repository

--- shopping-api/src/main/java/br/com/casadocodigo/shoppingapi/repository/ShopRepository.java
+++ shopping-api/src/main/java/br/com/casadocodigo/shoppingapi/repository/ShopRepository.java
@@ -3,5 +3,14 @@ package br.com.casadocodigo.shoppingapi.repository;
 import br.com.casadocodigo.shoppingapi.entity.Shop;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.List;
+
 public interface ShopRepository extends JpaRepository<Shop, Long> {
+    List<Shop> findAllByUserIdentifier(final String userIdentifier);
+
+    List<Shop> findAllByTotalGreaterThan(final BigDecimal total);
+
+    List<Shop> findAllByDateGreaterThanEquals(final LocalDateTime date);
 }
